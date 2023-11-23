68efc0cfb0a2b9896b5f5ecda87932c485556c45
Igor Baydikov <igor_baidikov@mial.ru>
2023-11-23 12:00:57 +0300

    -add code

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,29 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE students
+(
+	student_id serial PRIMARY KEY,
+	first_name varchar,
+	birthday date,
+	phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE students ADD COLUMN middle_name varchar
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE students DROP COLUMN middle_name
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE students RENAME COLUMN birthday TO birth_date
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE students ALTER COLUMN phone SET DATA TYPE varchar(32)
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO students (first_name, birth_date, phone) VALUES
+('Ivan', '2001-01-01', '111'),
+('Petr', '2002-02-02', '222'),
+('Nikolay', '2003-03-03', '333')
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE students RESTART IDENTITY
